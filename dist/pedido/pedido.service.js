d0d498456815fb77e2f4014b927b2a2254c7c687
Weldon Thallysson <weldonthallysson.dev@gmail.com>
2025-03-15 22:04:14 -0300

    integrações

--- dist/pedido/pedido.service.js
+++ dist/pedido/pedido.service.js
@@ -68,12 +68,10 @@ let PedidoService = class PedidoService {
             queryBuilder.andWhere('item.valor_unitario = :value', { value });
         }
         if (search) {
-            queryBuilder.andWhere('cliente.nome LIKE :clienteName', {
-                clienteName: `%${search}%`,
-            });
-        }
-        if (search) {
-            queryBuilder.andWhere('item.nome LIKE :search', { search: `%${search}%` });
+            queryBuilder.andWhere(new typeorm_2.Brackets((qb) => {
+                qb.where('cliente.nome LIKE :search', { search: `%${search}%` })
+                    .orWhere('item.nome LIKE :search', { search: `%${search}%` });
+            }));
         }
         return await queryBuilder.getMany();
     }
